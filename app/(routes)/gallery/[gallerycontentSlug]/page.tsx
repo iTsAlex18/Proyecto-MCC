ca33a3f8dca03d2f4212d54b7deaf8e60f858547
iTsAlex18 <jesus.bracamontediaz@cananea.tecnm.mx>
2025-05-30 16:03:05 -0700

    description

--- app/(routes)/gallery/[gallerycontentSlug]/page.tsx
+++ app/(routes)/gallery/[gallerycontentSlug]/page.tsx
@@ -84,9 +84,9 @@ return (
                                         {room.contentTitle}
                                     </h3>
                                 )}
-                                <p className="text-base sm:text-lg md:text-xl text-gray-800 leading-relaxed text-justify">
+                                <pre className="whitespace-pre-wrap text-base sm:text-lg md:text-xl text-gray-800 leading-relaxed text-center font-serif bg-gradient-to-br from-amber-200 to-rose-100 px-6 py-8 rounded-xl shadow-inner">
                                     {room.description}
-                                </p>
+                                </pre>
                             </div>
                         </div>
 
